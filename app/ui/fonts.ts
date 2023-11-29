700cd3976e95e0394ccd6fb6e382b4a261c66464
Javier <javier.sanchez@flecharoja.com>
2023-11-29 10:38:39 -0600

    add lusitana

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,7 +1,8 @@
-import { Montserrat, Inter} from 'next/font/google'
+import { Montserrat, Inter, Lusitana} from 'next/font/google'
 
 export const monserrat = Montserrat({subsets: ['latin']})
 
 export const inter = Inter({subsets: ['latin']})
+export const lusitana = Lusitana({subsets: ['latin'], weight: ['400','700']})
 
  
